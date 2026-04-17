d85a782ef942dc0394ab11505c3168bf7fd38828
NISHCHALA <70499863+nishunigam@users.noreply.github.com>
2026-04-17 09:21:45 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -100,4 +100,4 @@ def get_recommendations():
 # RUN SERVER
 # -------------------------------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='127.0.0.1', port=5000, debug=True)
